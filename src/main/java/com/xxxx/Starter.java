4ad3837ebe4207b257639b5d0d793e3cf92f1f4e
baixiaopang <2986085560@qq.com>
2021-01-22 20:41:04 +0800

    Initial commit

---
+++ src/main/java/com/xxxx/Starter.java
@@ -0,0 +1,15 @@
+package com.xxxx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author arthur
+ * @date 2021/1/22 20:36
+ */
+@SpringBootApplication
+public class Starter {
+    public static void main(String[] args) {
+        SpringApplication.run(Starter.class);
+    }
+}
---
+++ src/main/java/com/xxxx/crm/controller/IndexController.java
@@ -0,0 +1,41 @@
+package com.xxxx.crm.controller;
+
+import com.xxxx.base.BaseController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author arthur
+ * @date 2021/1/22 20:35
+ */
+@Controller
+public class IndexController extends BaseController {
+
+    /**
+     * 系统登录页
+     * @return
+     */
+    @RequestMapping("index")
+    public String index(){
+        return "index";
+    }
+
+
+    /**
+     * 系统界面欢迎页
+     * @return
+     */
+    @RequestMapping("welcome")
+    public String welcome(){
+        return "welcome";
+    }
+
+    /**
+     * 后端管理主页面
+     * @return
+     */
+    @RequestMapping("main")
+    public String main(){
+        return "main";
+    }
+}
