2c1fe6b1d8e33fb74b6f5b574a60a672559d1249
baixiaopang <2986085560@qq.com>
2021-01-22 21:11:49 +0800

    登录：校验数据

---
+++ src/main/java/com/xxxx/crm/service/UserService.java
@@ -0,0 +1,12 @@
+package com.xxxx.crm.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * service：
+ * @author arthur
+ * @date 2021/1/22 21:10
+ */
+@Service
+public class UserService {
+}
